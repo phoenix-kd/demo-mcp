ebd109dcdf6a2dfcfded585f0d5738682c934c58
phoenix <kuvamdevgan@gmail.com>
2025-04-28 00:21:22 +0530

    permissions

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "===== Activating virtual environment ====="
-source venv/bin/activate
-
-echo "===== Starting the mcp Server Application ====="
-exec python3 officesupply.py
